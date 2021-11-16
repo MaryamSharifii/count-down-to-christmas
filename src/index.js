e6b072b94f56cc98844a952f2f735bbfb9e5ac55
MaryamSharifii <maryam.1999.853@gmail.com>
2021-11-16 22:35:29 +0330

    add a little style

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ import './index.css';
 
 
 function countDown(){
-  <h1> countDown to christmas 2022</h1>
+  
   let countDownDate = new Date("Dec 24, 2021 23:59:01").getTime();
   
   let update = setInterval(function() {
-
+    
     let now = new Date().getTime();
     let distance = countDownDate - now;
 
@@ -16,9 +16,8 @@ function countDown(){
     let hours = Math.floor(distance % (1000 * 60 * 60 * 24) / (1000 * 60 * 60));
     let minutes = Math.floor(distance % (1000 * 60 * 60) / (1000 * 60));
     let seconds = Math.floor(distance % (1000 * 60) / 1000);
-
-
-    document.getElementById("root").innerHTML = days + "days" + hours + "hours" + minutes + "minutes" + seconds + "seconds";
+    
+    document.getElementById("root").innerHTML = days + " days " + hours + " hours " + minutes + " minutes " + seconds + " seconds ";
 
     if (distance < 0) {
       clearInterval(update);
