fb8eabce4b6e00f0dbe7db4b26fd8c68351eb9d2
MaryamSharifii <maryam.1999.853@gmail.com>
2021-11-16 22:16:08 +0330

    create the components

--- src/index.js
+++ src/index.js
@@ -1,17 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+
+function countDown(){
+  <h1> countDown to christmas 2022</h1>
+  let countDownDate = new Date("Dec 24, 2021 23:59:01").getTime();
+  
+  let update = setInterval(function() {
+
+    let now = new Date().getTime();
+    let distance = countDownDate - now;
+
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor(distance % (1000 * 60 * 60 * 24) / (1000 * 60 * 60));
+    let minutes = Math.floor(distance % (1000 * 60 * 60) / (1000 * 60));
+    let seconds = Math.floor(distance % (1000 * 60) / 1000);
+
+
+    document.getElementById("root").innerHTML = days + "days" + hours + "hours" + minutes + "minutes" + seconds + "seconds";
+
+    if (distance < 0) {
+      clearInterval(update);
+      document.getElementById("root").innerHTML = "EXPIRED!";
+    }
+
+  },1000);
+}
+countDown(); 
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <countDown/>,
+
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
